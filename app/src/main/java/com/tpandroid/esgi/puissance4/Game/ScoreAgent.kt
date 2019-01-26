7a83bff664faf2739749d1b054882ee7261724d0
Alexandre Lairan <lairana@free.fr>
2019-01-26 17:59:31 +0100

    Implement scoring system

---
+++ app/src/main/java/com/tpandroid/esgi/puissance4/Game/ScoreAgent.kt
@@ -0,0 +1,114 @@
+package com.tpandroid.esgi.puissance4.Game
+
+abstract class ScoreAgent(val board: Array<Board.Token>, val x: Int, val y: Int) {
+    abstract fun calculate(actual : Board.Token) : Int
+}
+
+class CenterAgent(board: Array<Board.Token>, x: Int, y: Int) : ScoreAgent(board, x, y) {
+    companion object {
+        val INCREMENTER = 4
+    }
+
+    override fun calculate(actual : Board.Token) : Int {
+        var counter = 0
+        repeat(y) {
+            if(board[(x / 2 ) + 1 + it * x] == actual) { counter += INCREMENTER }
+        }
+
+        return counter
+    }
+}
+
+class CountAgent(board: Array<Board.Token>, x: Int, y: Int) : ScoreAgent(board, x, y) {
+    companion object {
+        val INCREMENT_COUNTER = mapOf(
+            Pair(0, 0),
+            Pair(1, 0),
+            Pair(2, 2),
+            Pair(3, 5),
+            Pair(4, 1000)
+        )
+    }
+
+    override fun calculate(actual: Board.Token): Int {
+        var counter = 0
+        for(i in 0..(x - 1)) {
+            for(j in 0..(y - 1)) {
+                val piece = board[i * 7 + j]
+                if(piece != actual) { continue }
+
+                counter += horizontal(i, j, 1, actual)
+                counter += vertical(i, j, 1, actual)
+                counter += diagonal(i, j, 1, actual)
+                counter += diagonal(i, j, -1, actual)
+            }
+        }
+
+        return counter
+    }
+
+    private fun horizontal(i : Int, j : Int, direction: Int, actual: Board.Token) : Int {
+        var counter = 0
+
+        var threshold = 0
+        for(k in 0..3) {
+            if(j + k * direction >= 7 || j + k * direction < 0) { break }
+            val encounter = board[i * 7 + j + k * direction]
+
+            if(encounter == actual) {
+                threshold += 1
+
+                counter -= INCREMENT_COUNTER[threshold - 1] ?: 0
+                counter += INCREMENT_COUNTER[threshold] ?: 0
+            } else if(encounter == Board.Token.Empty) {
+                continue
+            } else {
+                break
+            }
+        }
+
+        return counter
+    }
+
+    private fun vertical(i : Int, j : Int, direction: Int, actual: Board.Token) : Int {
+        var counter = 0
+
+        var threshold = 0
+        for(k in 0..3) {
+            if(i + k * direction >= 6 || i + k * direction < 0) { break }
+            val encounter = board[(i + k * direction) * 7 + j]
+
+            if(encounter == actual) {
+                threshold += 1
+
+                counter -= INCREMENT_COUNTER[threshold - 1] ?: 0
+                counter += INCREMENT_COUNTER[threshold] ?: 0
+            } else if(encounter == Board.Token.Empty) {
+                continue
+            } else {
+                break
+            }
+        }
+
+        return counter
+    }
+
+    private fun diagonal(i : Int, j : Int, direction: Int, actual: Board.Token) : Int {
+        var threshold = 0
+        for(k in 0..3) {
+            if(i + k >= 6 || i + k * direction < 0) { break }
+            if(j + k * direction >= 7 || j + k * direction < 0) { break }
+            val encounter = board[(i + k) * 7 + (j + k * direction)]
+
+            if(encounter == actual) {
+                threshold += 1
+            } else if(encounter == Board.Token.Empty) {
+                continue
+            } else {
+                break
+            }
+        }
+
+        return INCREMENT_COUNTER[threshold] ?: 0
+    }
+}
---
+++ app/src/test/java/com/tpandroid/esgi/puissance4/Game/Score/CountAgentTest.kt
@@ -0,0 +1,139 @@
+package com.tpandroid.esgi.puissance4.Game.Score
+
+import com.tpandroid.esgi.puissance4.Game.Board
+import com.tpandroid.esgi.puissance4.Game.CountAgent
+import org.junit.Assert
+import org.junit.Test
+
+class CountAgentTest {
+    @Test
+    fun withNoToken() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(0, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withOneToken() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[4 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(0, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoNonAlignedTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[5 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(0, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoAlignedHorizontalTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[1 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(2, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoAlignedHorizontalWithGapeTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[2 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(2, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoAlignedHorizontalWithInterceptionTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[1 + 5 * 7] = Board.Token.Player2
+        board[2 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(0, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoAlignedVerticalTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[0 + 4 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(2, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoAlignedVerticalWithInterceptionTokens() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[0 + 4 * 7] = Board.Token.Player2
+        board[0 + 3 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(0, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withVerticalAndHorizontalAndDiagonal() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[0 + 4 * 7] = Board.Token.Player1
+        board[1 + 5 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(6, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withTwoDiagonal() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[1 + 4 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(2, subject.calculate(Board.Token.Player1))
+    }
+
+    @Test
+    fun withThreeDiagonal() {
+        val board = Array(6 * 7) { Board.Token.Empty }
+        board[0 + 5 * 7] = Board.Token.Player1
+        board[1 + 4 * 7] = Board.Token.Player1
+        board[2 + 3 * 7] = Board.Token.Player1
+        val subject = CountAgent(board, 6, 7)
+
+        Assert.assertEquals(5 + 2, subject.calculate(Board.Token.Player1))
+    }
+
+    fun toString(board : Array<Board.Token>) : String {
+        val builder = StringBuilder();
+
+        builder.append("----------\n")
+        repeat(6) { i ->
+            repeat(7) { j ->
+                val piece = board[i * 7 + j]
+                var value = '.'
+
+                if(piece == Board.Token.Player1) { value = '1' }
+                if(piece == Board.Token.Player2) { value = '2' }
+
+                builder.append("| ${value} ")
+            }
+            builder.append("\n----------\n")
+        }
+
+        return builder.toString()
+    }
+}
