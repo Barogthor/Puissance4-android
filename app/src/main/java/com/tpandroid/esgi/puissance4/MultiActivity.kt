ecd39d67655a5bc9de7dec866d0d8fc7d93c4a0c
Unknown <f.thery3103@gmail.com>
2019-02-06 17:10:36 +0100

    Link offline to gameboard

--- app/src/main/java/com/tpandroid/esgi/puissance4/MultiActivity.kt
+++ app/src/main/java/com/tpandroid/esgi/puissance4/MultiActivity.kt
@@ -1,12 +1,23 @@
 package com.tpandroid.esgi.puissance4
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MultiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.multi_layout)
+    }
+
+    fun goToGameBoardOffline(view: View?){
+        var intent = Intent(this,PlayBoardActivity::class.java).apply {  }
+        startActivity(intent)
+    }
+
+    fun goToGameboardOnline(view: View?){
+
     }
 }
