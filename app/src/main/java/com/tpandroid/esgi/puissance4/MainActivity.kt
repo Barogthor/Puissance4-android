20e6dbe06f8953d496f71668d843476fc1a54d80
Nico C <nicococq25@gmail.com>
2019-02-24 15:09:37 +0100

    Connexion supposedly done

--- app/src/main/java/com/tpandroid/esgi/puissance4/MainActivity.kt
+++ app/src/main/java/com/tpandroid/esgi/puissance4/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.tpandroid.esgi.puissance4
 
-import android.content.Intent
+//import android.content.Intent
 import android.app.Activity
 import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
@@ -23,25 +23,28 @@ import com.google.android.gms.tasks.Task
 import android.support.annotation.NonNull
 import com.google.android.gms.tasks.OnCompleteListener
 import android.content.Intent
+import android.support.v4.app.FragmentActivity
+import android.widget.Button
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInResult
-
-
-
-
-
-
-
-
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.games.Games
 
 
 class MainActivity : AppCompatActivity() {
 
     /* Request code used to invoke sign in user interactions. */
-    private val RC_SIGN_IN = 0
+    private val RC_SIGN_IN = 9001
+
+    // Client used to sign in with Google APIs
+    private var mGoogleSignInClient: GoogleSignInClient? = null
 
     var database = FirebaseDatabase.getInstance()
     var myRef = database.getReference("message")
+
+    // tag for debug logging
+    private val TEST_RESULT = "TEST_RESULT"
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
@@ -49,8 +52,8 @@ class MainActivity : AppCompatActivity() {
     val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
     var signInOpt = signInOptions.build()
 
-    var gso = signInOptions.requestServerAuthCode("839161475425-40m7ffku0dsd3723dv3ph13njuuplosq.apps.googleusercontent.com")
-        .build()
+    /*var gso = signInOptions.requestServerAuthCode("839161475425-40m7ffku0dsd3723dv3ph13njuuplosq.apps.googleusercontent.com")
+        .build()*/
 
     var account = GoogleSignInAccount.createDefault()
 
@@ -60,7 +63,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_layout)
 
-        // Initialize Firebase Auth
+        startSignInIntent()
+
+        // Create the client used to sign in to Google services.
+        /*mGoogleSignInClient = GoogleSignIn.getClient(
+            this,
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build()
+        )*/
+
+        /*// Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
         // Obtain the FirebaseAnalytics instance.
@@ -79,12 +90,23 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.i("TEST_RESULT_ISSIGNIN :", "Is Not Sign In")
             startSignInIntent()
-        }
+        }*/
 
         //Unlock Achievements
         //bundle.putString(FirebaseAnalytics.Param.ACHIEVEMENT_ID, achievementId);
         //firebaseAnalytics.logEvent(FirebaseAnalytics.Event.UNLOCK_ACHIEVEMENT, bundle);
 
+    }
+
+    private fun startSignInIntent() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestServerAuthCode("839161475425-40m7ffku0dsd3723dv3ph13njuuplosq.apps.googleusercontent.com")
+            .requestProfile()
+            .build()
+
+        val signInClient = GoogleSignIn.getClient(this, gso)
+        val intent = signInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
     }
 
     fun goToSolo(view: View?){
@@ -104,10 +126,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        Log.i("TEST_RESULT :", "Blablabla22222")
-        var signInClient = GoogleSignIn.getClient(this,
+        Log.i(TEST_RESULT, "Blablabla22222")
+        /*var signInClient = GoogleSignIn.getClient(this,
             gso)
-        signInClient.silentSignIn()
+        signInClient.silentSignIn()*/
+
+        //signInSilently();
+
         /*if(isSignedIn())
             signInSilently()
         else {
@@ -116,39 +141,38 @@ class MainActivity : AppCompatActivity() {
         }*/
     }
 
-    private fun isSignedIn(): Boolean {
+    /*private fun isSignedIn(): Boolean {
         return GoogleSignIn.getLastSignedInAccount(this) != null
     }
 
     private fun signInSilently() {
-        var signInClient = GoogleSignIn.getClient(this, gso)
-        signInClient.silentSignIn().addOnCompleteListener(this
+        Log.d(TEST_RESULT, "signInSilently()")
+
+        mGoogleSignInClient!!.silentSignIn().addOnCompleteListener(this
         ) { task ->
             if (task.isSuccessful) {
-                // The signed in account is stored in the task's result.
-                val signedInAccount = task.result
-                Log.i("TEST_RESULT success :", signedInAccount.toString())
+                Log.d(TEST_RESULT, "signInSilently(): success")
+                //onConnected(task.result)
             } else {
-                // Player will need to sign-in explicitly using via UI
-                Log.i("TEST_RESULT failure :", task.exception.toString())
-                startSignInIntent()
+                Log.d(TEST_RESULT, "signInSilently(): failure", task.exception)
+                //onDisconnected()
             }
         }
-    }
+    }*/
 
-    private fun startSignInIntent() {
-        val signInClient = GoogleSignIn.getClient(
-            this,
-            gso
-        )
-        val intent = signInClient.signInIntent
-        startActivityForResult(intent, RC_SIGN_IN)
-    }
+    /*private fun startSignInIntent() {
+        startActivityForResult(mGoogleSignInClient!!.getSignInIntent(), RC_SIGN_IN)
+    }*/
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+            /*val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result.isSuccess) {
                 // The signed in account is stored in the result.
                 val signedInAccount = result.signInAccount
@@ -159,7 +183,22 @@ class MainActivity : AppCompatActivity() {
                 }
                 AlertDialog.Builder(this).setMessage(message)
                     .setNeutralButton(android.R.string.ok, null).show()
-            }
+            }*/
+        }
+    }
+
+    private fun handleSignInResult(completedTask:Task<GoogleSignInAccount>) {
+        try
+        {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account)
+        }
+        catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TEST_RESULT, "signInResult:failed code=" + e.getStatusCode())
+            //updateUI(null)
         }
     }
 }
